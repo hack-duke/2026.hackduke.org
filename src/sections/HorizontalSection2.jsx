567687486642cc7e768e72b76c6b3f3fe5874b50
Kelly Xu <kelly.xu@duke.edu>
2025-11-16 16:03:20 -0500

    new images, doesn't work yet

--- src/sections/HorizontalSection2.jsx
+++ src/sections/HorizontalSection2.jsx
@@ -1,41 +1,40 @@
 import bg from '/bg-horizontal.svg';
-import stairs from '/stairs.svg';
-import side2Floor from '/side-2-floor.svg';
+import table from '/table.svg';
 
-//TODO: rn looks ugly bc stairs super small, also separate out the floor from stairs 
 export default function HorizontalSection2() {
   return (
     <div
       style={{
-        position: 'relative', // this makes it a positioning context
-        width: '100%',
-        height: '400px',      // give it some height
-        overflow: 'hidden',   // optional: hides overflow if images are large
+        position: 'relative',
+        minHeight: '400px'
       }}
     >
+
       {/* Background image */}
       <img
         src={bg}
         alt="Background"
         style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
           width: '100%',
           height: '100%',
           objectFit: 'cover',
         }}
       />
 
-      {/* Stairs image overlay */}
       <img
-        src={stairs}
-        alt="Stairs"
+        src={table}
+        alt="Table"
         style={{
           position: 'absolute',
-          top: '50%',     // adjust positioning as needed
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          width: '200px', // or whatever fits your layout
+          bottom: '20%',     // adjust positioning as needed
+          transform: 'translateX(-50%)', // Centering correction
+          width: '80%', 
         }}
       />
+
     </div>
   );
 }
