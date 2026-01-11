43c4232ec9b61fbd6842c39e4bd328588bc6b35c
Humam Al-Shami <Humam.Al-Shami@Duke.edu>
2026-01-11 15:42:41 -0500

    Added MLHBadge

--- src/App.jsx
+++ src/App.jsx
@@ -103,6 +103,30 @@ function App() {
 
   return (
     <>
+      {/* MLH Trust Badge */}
+      <a
+        id="mlh-trust-badge"
+        style={{
+          display: 'block',
+          maxWidth: '100px',
+          minWidth: '60px',
+          position: 'absolute',
+          left: '10px',
+          top: '0',
+          width: '10%',
+          zIndex: 10000,
+        }}
+        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg"
+          alt="Major League Hacking 2026 Hackathon Season"
+          style={{ width: '100%' }}
+        />
+      </a>
+
       {/* Sticky header */}
       <div className={`sticky-header ${showHeader ? 'show' : 'hide'} ${mobileOpen ? 'overlay-open' : ''}`}>
         <div className="header-items">
