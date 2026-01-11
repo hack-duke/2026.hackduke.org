63051719faa74e01f4bc00298611ab5f784ee71e
Humam Al-Shami <Humam.Al-Shami@Duke.edu>
2026-01-11 17:46:24 -0500

    Navbar Countdown Added

---
+++ src/components/CountdownRenderer.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+const CountdownRenderer = ({ days, hours, minutes, seconds, deadlineType }) => {
+  return (
+    <div className="hero-countdown">
+      <p>
+        {deadlineType} close in {days} days {hours} hrs {minutes} min{" "}
+        {seconds} sec
+      </p>
+    </div>
+  );
+};
+
+// this part was required by the linter?
+CountdownRenderer.propTypes = {
+  days: PropTypes.number.isRequired,
+  hours: PropTypes.number.isRequired,
+  minutes: PropTypes.number.isRequired,
+  seconds: PropTypes.number.isRequired,
+  deadlineType: PropTypes.string.isRequired,
+};
+
+export default CountdownRenderer;
