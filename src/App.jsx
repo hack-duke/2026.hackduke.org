fc7fc3e119c929b0072948b48fe2b937f9ad367f
Humam Al-Shami <Humam.Al-Shami@Duke.edu>
2026-01-13 21:28:00 -0500

    Fixed Regular Application Deadline Date

--- src/App.jsx
+++ src/App.jsx
@@ -118,7 +118,7 @@ function App() {
           <img src={countdownSvg} alt="" className="countdown-background" />
           <div className="countdown-text">
             <Countdown
-              date={new Date('2026-01-16T00:00:00')}
+              date={new Date('2026-01-16T23:59:59-05:00')}
               renderer={(props) => (
                 <CountdownRenderer {...props} deadlineType="Applications" />
               )}
